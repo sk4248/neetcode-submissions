514e55442d768797ab460686c011c29e34ca642a
sreekanth Reddy <srikanth6006@gmail.com>
2026-04-25 17:02:29 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+      const map = new Map();
+      for(let str of strs){
+        let key = str.split('').sort().join('')
+        if(!map.get(key)){
+            map.set(key, [])
+        }
+        map.get(key).push(str)
+      }
+      return Array.from(map.values())
+        
+    }
+}
