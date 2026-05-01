6ae84e408d7163750c6ce07f8bed7e66ec868b27
sreekanth Reddy <srikanth6006@gmail.com>
2026-04-30 20:43:57 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const string = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        console.log(string);
+        let i=0;
+        let j=string.length-1;
+        while(j>=i){
+            if(string[i] === string[j]){
+                i++;
+                j--;
+            }else{
+                return false
+            }
+        }
+        return true;
+    }
+}
