50cd59e577d8cc2454b9ec945e0b7e76b2892b00
sreekanth Reddy <srikanth6006@gmail.com>
2026-08-05 17:55:20 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length-1
+
+    while(start <= end){
+        let middle = Math.floor((start+end)/2)
+
+        if(nums[middle] === target){
+            return middle;
+        } else if(nums[middle] > target){
+            end = middle - 1;
+        } else {
+          start = middle + 1;
+        }
+     
+    }
+       return -1;
+    }
+}
