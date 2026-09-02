66fd1f9d2687c5514d526f20a8d57694407f26af
sreekanth Reddy <srikanth6006@gmail.com>
2026-09-01 19:40:11 -0500

    Add: subtree-of-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-3.js
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if(!p || !q) return !p === !q;
+
+        const q1 = [p];
+        const q2 = [q];
+        let node1;
+        let node2;
+        while(q1.length > 0 || q2.length > 0){
+            node1 = q1.shift();
+            node2 = q2.shift();
+            if(node1.val !== node2.val){
+                return false;
+            }
+            if(node1.left && node2.left){
+                q1.push(node1.left);
+                q2.push(node2.left);
+            } else if (node1.left !== node2.left){
+                return false
+            } 
+
+            if(node1.right && node2.right){
+                q1.push(node1.right);
+                q2.push(node2.right);
+            } else if (node1.right !== node2.right){
+                return false
+            } 
+        }
+        return true;
+        
+    }
+ isSubtree(root, subRoot) {
+    if (!root) return false;
+
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+
+        if (node.val === subRoot.val && this.isSameTree(node, subRoot)) {
+            return true;
+        }
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return false;
+}
+}
